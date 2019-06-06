9c3c6011c4eae0bb8a13d1db5258ea25bb5b40b0
Alexander Cavalheiro Becker <callfrygou@hotmail.com>
2019-06-05 22:06:54 -0300

    Creates the basic files

---
+++ system/admin/tela_cadastro_pontos.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+
+  <title>Admin</title>
+
+  <link rel="stylesheet" type="text/css" href="../../css/admin.css">
+</head>
+
+<body>
+  <div class="container">
+   
+  </div>
+</body>
+</html>
