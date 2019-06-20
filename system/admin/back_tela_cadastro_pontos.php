68f21d1cc4895434233d72e6c773cef975844c07
Alexander Cavalheiro Becker <Alexander Cavalheiro Becker>
2019-06-19 21:49:34 -0300

    WORKING FOR INDIVIDUAL GOALS

--- system/admin/back_tela_cadastro_pontos.php
+++ system/admin/back_tela_cadastro_pontos.php
@@ -2,51 +2,45 @@
 session_start();
 include "../../database/conexao_bd.php";
 
-$idCampanha = $_POST['idCampanha'];
-$tipoCampanha = $_POST['tipoCampanha'];
 $idColaborador = $_POST['idColaborador'];
-$cpf = $_POST['cpf'];
 $valor = $_POST['valor'];
 $quantidade = $_POST['quantidade'];
-var_dump($idCampanha);
-var_dump($tipoCampanha);
-var_dump($idColaborador);
-var_dump($cpf);
 $link     = "navbar.php?folder=&file=tela_cadastro_pontos.php";
 $mensagem = "";
 
-$queryMetas = mysqli_query($con, "SELECT * FROM `metas` WHERE metas.campanhas_id = '$idCampanha'");
-$metas = mysqli_fetch_all($queryMetas, MYSQLI_ASSOC);
+// $queryMetas = mysqli_query($con, "SELECT * FROM `metas` LEFT OUTER JOIN (select distinct id, campanhas.tipo_participantes from campanhas) c ON c.id=metas.campanhas_id");
+// $metas = mysqli_fetch_all($queryMetas, MYSQLI_ASSOC);
 
-if ($tipoCampanha == 0) {
-	$queryMetasIndividuais = mysqli_query($con, "SELECT * FROM metas INNER JOIN metas_has_colaboradores ON metas_has_colaboradores.metas_id = metas.id WHERE metas_has_colaboradores.colaboradores_id = '$idColaborador'");
-	// $campanhasIndividuais = mysqli_fetch_all($queryMetasIndividuais, MYSQLI_ASSOC);
-	foreach ($queryMetasIndividuais as $metasIndividuais) {
-		if($metasIndividuais['tipo'] == 1) {
-			//valor
-			$idMeta = $metasIndividuais['id'];
-			$objetivoMeta = $metasIndividuais['objetivo'];
-			$variantePontos = $metasIndividuais['variante_pontos'];
-			$bonificacao = $metasIndividuais['bonificacao'];
-			$valoresParaMultiplicarValor = round($valor/$variantePontos);
-			$pontosDeValor = $bonificacao * $valoresParaMultiplicarValor;
-			$atualizaPontosValor = mysqli_query($con,"UPDATE metas_has_colaboradores AS mc INNER JOIN colaboradores AS c ON mc.colaboradores_id=c.id SET mc.objetivo_conquistado = mc.objetivo_conquistado + $valor, mc.pontos_conquistados = mc.pontos_conquistados + $pontosDeValor, c.pontos = c.pontos + $pontosDeValor, c.creditos = c.creditos + $pontosDeValor WHERE mc.metas_id = $idMeta AND mc.colaboradores_id = $idColaborador");
-			$atualizaStatusMetaValor = mysqli_query($con, "UPDATE metas_has_colaboradores SET metas_has_colaboradores.status = 1 WHERE metas_has_colaboradores.objetivo_conquistado = $objetivoMeta");
-		} else if($metasIndividuais['tipo'] == 2) {
-			//quantidade
-			$idMeta = $metasIndividuais['id'];
-			$objetivoMeta = $metasIndividuais['objetivo'];
-			$variantePontos = $metasIndividuais['variante_pontos'];
-			$bonificacao = $metasIndividuais['bonificacao'];
-			$valoresParaMultiplicarQuantidade = round($quantidade/$variantePontos);
-			$pontosDeQuantidade = $bonificacao * $valoresParaMultiplicarQuantidade;
-			$atualizaPontosQuantidade = mysqli_query($con, "UPDATE metas_has_colaboradores AS mc INNER JOIN colaboradores AS c ON mc.colaboradores_id=c.id SET mc.objetivo_conquistado = mc.objetivo_conquistado + $quantidade, mc.pontos_conquistados = mc.pontos_conquistados + $pontosDeQuantidade, c.pontos = c.pontos + $pontosDeQuantidade, c.creditos = c.creditos + $pontosDeQuantidade WHERE mc.metas_id = $idMeta AND mc.colaboradores_id = $idColaborador");
-			// UPDATE metas_has_colaboradores AS mc INNER JOIN colaboradores AS c ON mc.colaboradores_id=c.id SET mc.objetivo_conquistado = mc.objetivo_conquistado + 1, mc.pontos_conquistados = mc.pontos_conquistados + 1, c.pontos = c.pontos + 1, c.creditos = c.creditos + 1 WHERE mc.metas_id = 3 AND mc.colaboradores_id = 3
-			$atualizaStatusMetaQuantidade = mysqli_query($con, "UPDATE metas_has_colaboradores SET metas_has_colaboradores.status = 1 WHERE metas_has_colaboradores.objetivo_conquistado = $objetivoMeta");
-		}
+$queryMetasIndividuais = mysqli_query($con, "SELECT * FROM metas INNER JOIN metas_has_colaboradores ON metas_has_colaboradores.metas_id = metas.id LEFT OUTER JOIN (select distinct id, campanhas.tipo_participantes from campanhas)c ON c.id=metas.campanhas_id WHERE metas_has_colaboradores.colaboradores_id = $idColaborador AND c.tipo_participantes = 0");
+foreach ($queryMetasIndividuais as $metasIndividuais) {
+	// var_dump($metasIndividuais);
+	if($metasIndividuais['tipo'] == 1) {
+		//valor
+		$idMeta = $metasIndividuais['metas_id'];
+		$objetivoMeta = $metasIndividuais['objetivo'];
+		$variantePontos = $metasIndividuais['variante_pontos'];
+		$bonificacao = $metasIndividuais['bonificacao'];
+		$valoresParaMultiplicarValor = round($valor/$variantePontos);
+		$pontosDeValor = $bonificacao * $valoresParaMultiplicarValor;
+		$atualizaPontosValor = mysqli_query($con,"UPDATE metas_has_colaboradores AS mc INNER JOIN colaboradores AS c ON mc.colaboradores_id=c.id SET mc.objetivo_conquistado = mc.objetivo_conquistado + $valor, mc.pontos_conquistados = mc.pontos_conquistados + $pontosDeValor, c.pontos = c.pontos + $pontosDeValor, c.creditos = c.creditos + $pontosDeValor WHERE mc.metas_id = $idMeta AND mc.colaboradores_id = $idColaborador");
+		$atualizaStatusMetaValor = mysqli_query($con, "UPDATE metas_has_colaboradores SET metas_has_colaboradores.status = 1 WHERE metas_has_colaboradores.objetivo_conquistado = $objetivoMeta");
+	} else if($metasIndividuais['tipo'] == 2) {
+		//quantidade
+		$idMeta = $metasIndividuais['metas_id'];
+		$objetivoMeta = $metasIndividuais['objetivo'];
+		echo $objetivoMeta;
+		$variantePontos = $metasIndividuais['variante_pontos'];
+		$bonificacao = $metasIndividuais['bonificacao'];
+		$valoresParaMultiplicarQuantidade = round($quantidade/$variantePontos);
+		$pontosDeQuantidade = $bonificacao * $valoresParaMultiplicarQuantidade;
+		$atualizaPontosQuantidade = mysqli_query($con, "UPDATE metas_has_colaboradores AS mc INNER JOIN colaboradores AS c ON mc.colaboradores_id=c.id SET mc.objetivo_conquistado = mc.objetivo_conquistado + '$quantidade', mc.pontos_conquistados = mc.pontos_conquistados + '$pontosDeQuantidade', c.pontos = c.pontos + '$pontosDeQuantidade', c.creditos = c.creditos + '$pontosDeQuantidade' WHERE mc.metas_id = '$idMeta' AND mc.colaboradores_id = '$idColaborador'");			
+		$atualizouPontos = mysqli_affected_rows($con);
+		$atualizaStatusMetaQuantidade = mysqli_query($con, "UPDATE metas_has_colaboradores SET metas_has_colaboradores.status = 1 WHERE metas_has_colaboradores.objetivo_conquistado = $objetivoMeta");
+		$atualizouStatusMeta = mysqli_affected_rows($con);
+		echo $atualizouStatusMeta;
 	}
-	$mensagem = "Campanha individual";
 }
+$mensagem = "Campanha individual";
 
 // header("Location: ".$link."&mensagem=".$mensagem);
 ?>
--- system/admin/tela_cadastro_pontos.php
+++ system/admin/tela_cadastro_pontos.php
@@ -14,63 +14,6 @@
   $campanhas = mysqli_fetch_all($result, MYSQLI_ASSOC);
   ?>
   <div class="borda" style=" margin-top:3%;">
-    <form action="" method="POST">
-      <div class="form-row align-items-center">
-        <div class="col-auto my-1">
-          <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Campanhas</label>
-          <select class="custom-select mr-sm-2 select_campanha" name="campanhas" id="campanhas">
-            <option>Selecione uma campanha</option>
-            <?php foreach($campanhas as $campanha){ ?>
-              <option  value="<?php echo $campanha["id"]; $id=$campanha["id"]?>" ><?php echo $campanha["nome"]; ?></option>
-              <?php } ?>
-            </select>
-          </div>
-          <button type="submit" class="btn btn-success botao" name="editar">Ver</button>
-        </div>
-      </form>
-        <div  style="overflow-y:auto; max-height:60vh;">
-          <h4 style="text-align:center;">Informações da Campanha</h4>
-          <hr>
-          <table class="table table-hover overflow-y">
-            <thead  class="thead-dark">
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Descrição</th>
-                <th scope="col">Início</th>
-                <th scope="col">Fim</th>
-                <th scope="col">Tipo de Participantes</th>
-              </tr>
-            </thead>
-            <tbody id="tabela">
-              <?php
-              if(isset($_POST) && $_POST['campanhas']!=""){
-                $sql = "SELECT * FROM campanhas WHERE id = " . $_POST['campanhas'];
-                $campanhas  = mysqli_query($con, $sql);
-              
-                foreach($campanhas AS $campanha):
-                $idCampanha= $campanha['id'];
-                $tipoCampanha = $campanha['tipo'];
-              ?>
-                <tr>
-                  <td><?php echo $campanha['id']; ?></td>
-                  <td><?php echo $campanha['nome']; ?></td>
-                  <td><?php echo $campanha['descricao']; ?></td>
-                  <td><?php echo $campanha['data_inicial']; ?></td>
-                  <td><?php echo $campanha['data_final']; ?></td>
-                  <?php if ($campanha['tipo_participantes'] == 0){ ?>
-                    <td><?php echo "Individual"; ?></td>
-                  <?php }else{ ?>
-                    <td><?php echo "Grupo"; ?></td>
-                  <?php } ?>
-                </tr>
-              <?php
-                endforeach;
-              }
-              ?>
-            </tbody>
-          </table>
-          <hr>
           <h4 style="text-align:center;">Participantes da campanha</h4>
           <hr>
           <table class="table table-hover overflow-y">
@@ -83,15 +26,12 @@
                 <th scope="col">Quantidade</th>
                 <th scope="col"></th>
                 <th scope="col"></th>
-                <th scope="col"></th>
-                <th scope="col"></th>
-                <th scope="col"></th>
               </tr>
             </thead>
             <tbody id="tabela">
               <?php
-              if(isset($_POST) && $_POST['campanhas']!=""){
-                $sql = "SELECT * FROM colaboradores INNER JOIN metas_has_colaboradores ON metas_has_colaboradores.colaboradores_id=colaboradores.id LEFT OUTER JOIN (select distinct id, metas.campanhas_id from metas) m ON m.id=metas_has_colaboradores.metas_id where m.campanhas_id = '$idCampanha' AND colaboradores.status = 0";
+              if(isset($_POST)){
+                $sql = "SELECT * FROM colaboradores INNER JOIN metas_has_colaboradores ON metas_has_colaboradores.colaboradores_id=colaboradores.id where colaboradores.status = 0";
                 $colaboradores  = mysqli_query($con, $sql);
 
                 foreach($colaboradores AS $colaborador):
@@ -108,17 +48,8 @@
                   <?php } ?>
                   <td><input type="number" class="form-control" id="valor" name="valor" aria-describedby="Valor" placeholder="Digite o valor" required></td>  
                   <td><input type="number" class="form-control" id="quantidade" name="quantidade" aria-describedby="Quantidade" placeholder="Digite a quantidade" required></td>
-                  <td><input type="hidden" class="form-control" id="cpf" name="cpf" value="<?php $colaborador['cpf'] ?>"></td>
-                  <td><input type="hidden" class="form-control" id="tipoCampanha" name="tipoCampanha" value="<?php $tipoCampanha ?>"></td>
-                  <td><input type="hidden" class="form-control" id="idCampanha" name="idCampanha" value="<?php $idCampanha ?>"></td> 
-                  <td><input type="hidden" class="form-control" id="idColaborador" name="idColaborador" value="<?php $idColaborador ?>"></td>  
+                  <td><input type="hidden" class="form-control" id="idColaborador" name="idColaborador" value="<?php echo $idColaborador ?>"></td>  
                   <td><button type="submit" class="btn btn-success botao" name="editar">Enviar</button></td>
-                  <?php 
-                  var_dump($idCampanha);
-                  var_dump($tipoCampanha);
-                  var_dump($idColaborador);
-                  var_dump($cpf);
-                  ?>
                 </tr>
               </form>
               <?php
