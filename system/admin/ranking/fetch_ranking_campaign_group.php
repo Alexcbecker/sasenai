a759f18d2db5d83aca7383768b6d4b813784096e
adriano <adriano.souza113@gmail.com>
2019-06-24 12:29:03 -0300

    fix query camp id

--- system/admin/ranking/fetch_ranking_campaign_group.php
+++ system/admin/ranking/fetch_ranking_campaign_group.php
@@ -16,10 +16,11 @@ $q = <<<S
 SELECT grupos.nome AS nome, c.pontos AS pontos FROM grupos
 INNER JOIN (SELECT grupos_id AS id, SUM(s.pontos) AS pontos 
 FROM (SELECT colaboradores_has_grupos_id AS id, SUM(pontos_conquistados) AS pontos FROM metas_has_colaboradores_has_grupos
-LEFT JOIN (SELECT id FROM metas WHERE metas.campanhas_id = 1) AS m ON m.id = metas_has_colaboradores_has_grupos.metas_id
+LEFT JOIN (SELECT id FROM metas WHERE metas.campanhas_id = '{$_GET['id']}') AS m ON m.id = metas_has_colaboradores_has_grupos.metas_id
 GROUP BY metas_has_colaboradores_has_grupos.colaboradores_has_grupos_id) AS s
 RIGHT JOIN colaboradores_has_grupos ON s.id = colaboradores_has_grupos.id
 GROUP BY colaboradores_has_grupos.grupos_id) AS c ON c.id = grupos.id
+WHERE grupos.status = 0
 ORDER BY c.pontos DESC
 S;
 
