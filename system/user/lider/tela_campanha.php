a361710e3db0cdfa866123c5d864699f79701355
Alexander Cavalheiro Becker <callfrygou@hotmail.com>
2019-06-25 21:25:58 -0300

    Leader campaign screen almost done

--- system/user/lider/tela_campanha.php
+++ system/user/lider/tela_campanha.php
@@ -11,12 +11,18 @@
     $id_usuario = $_SESSION['id'];
 
   include "../../database/conexao_bd.php";
-  // SELECT * FROM campanhas INNER JOIN metas ON metas.campanhas_id=campanhas.id INNER JOIN metas_has_colaboradores ON metas_has_colaboradores.metas_id=metas.id WHERE metas_has_colaboradores.colaboradores_id = 3
-  // SELECT * FROM campanhas INNER JOIN metas ON metas.campanhas_id=campanhas.id INNER JOIN metas_has_colaboradores_has_grupos on metas_has_colaboradores_has_grupos.metas_id=metas.id INNER JOIN colaboradores_has_grupos ON colaboradores_has_grupos.id=metas_has_colaboradores_has_grupos.colaboradores_has_grupos_id WHERE colaboradores_has_grupos.colaboradores_id = 3
-  $sql_sel =  "SELECT * FROM campanhas INNER JOIN colaboradores_has_campanhas ON colaboradores_has_campanhas.campanhas_id=campanhas.id WHERE colaboradores_has_campanhas.colaboradores_id ='$id_usuario' ";
-  $result  = mysqli_query($con, $sql_sel);
-  if (!$result) die ("Erro ao conectar usuário.");
-  $campanhas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $queryCampanhasIndividuais = "SELECT DISTINCT * FROM campanhas INNER JOIN (SELECT id as metasId, metas.campanhas_id from metas)m ON m.campanhas_id=campanhas.id INNER JOIN (SELECT metas_id, metas_has_colaboradores.colaboradores_id from metas_has_colaboradores) mc ON mc.metas_id=m.metasId WHERE mc.colaboradores_id = '$id_usuario'";
+  $queryCampanhasGrupo = "SELECT DISTINCT * FROM campanhas INNER JOIN (SELECT id AS metasId, metas.campanhas_id from metas) m ON m.campanhas_id=campanhas.id INNER JOIN (SELECT metas_has_colaboradores_has_grupos.metas_id, metas_has_colaboradores_has_grupos.colaboradores_has_grupos_id from metas_has_colaboradores_has_grupos) mg on mg.metas_id=m.metasId INNER JOIN (SELECT id AS metasCol, colaboradores_has_grupos.colaboradores_id from colaboradores_has_grupos) cg ON cg.metasCol=mg.colaboradores_has_grupos_id WHERE cg.colaboradores_id = '$id_usuario'";
+  $arrayCampanhas = array();
+  $resultadoIndividuais = mysqli_query($con, $queryCampanhasIndividuais);
+  $resultadoGrupos = mysqli_query($con, $queryCampanhasGrupo);
+  while($linha = mysqli_fetch_array($resultadoIndividuais, MYSQLI_ASSOC)) {
+    array_push($arrayCampanhas, $linha);
+  }
+
+  while($sos = mysqli_fetch_array($resultadoGrupos, MYSQLI_ASSOC)) {
+    array_push($arrayCampanhas, $sos);
+  }
   ?>
   <div class="borda" style=" margin-top:3%;">
     <form action="" method="POST">
@@ -25,7 +31,8 @@
           <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Campanhas</label>
           <select class="custom-select mr-sm-2 select_campanha" name="campanhas" id="campanhas">
             <option>Selecione uma campanha</option>
-            <?php foreach($campanhas as $campanha){ ?>
+            <?php foreach($arrayCampanhas as $campanha){?>
+           
               <option  value="<?php echo $campanha["id"]; $id=$campanha["id"]?>" ><?php echo $campanha["nome"]; ?></option>
               <?php } ?>
             </select>
@@ -46,8 +53,6 @@
               <th scope="col">Nome</th>
               <th scope="col">Descrição</th>
               <th scope="col">Tipo</th>
-              <th scope="col">Bonificação</th>
-              <th scope="col">Variante pontos</th>
               <th scope="col">Data inicial</th>
               <th scope="col">Data final</th>
               <th scope="col">Tipo participantes</th>
@@ -70,8 +75,6 @@
                     <?php }else{ ?>
                       <td><?php echo "Quantidade"; ?></td>
                       <?php } ?>
-                      <td><?php echo $campanha['bonificacao']; ?></td>
-                      <td><?php echo $campanha['variante_pontos']; ?></td>
                       <td><?php echo date('d/m/Y', strtotime($campanha['data_inicial'])); ?></td>
                       <td><?php echo date('d/m/Y', strtotime($campanha['data_final'])); ?></td>
                       <?php if ($campanha['tipo_participantes'] == 0){ ?>
@@ -98,7 +101,10 @@
                     <tr>
                       <th scope="col">#</th>
                       <th scope="col">Nome</th>
-                      <th scope="col">descricao</th>
+                      <th scope="col">Descrição</th>
+                      <th scope="col">Tipo</th>
+                      <th scope="col">Bonificação</th>
+                      <th scope="col">Variante pontos</th>
                       <th scope="col">Pontos</th>
                       <th scope="col">Objetivo</th>
                       <th scope="col">Status</th>
@@ -108,32 +114,38 @@
                     <?php
                     if(isset($_POST) && $_POST['campanhas']!=""){
                       $sql = "SELECT * FROM `metas` WHERE  campanhas_id = " . $_POST['campanhas'];
-                      $meta  = mysqli_query($con, $sql);
+                      $metas  = mysqli_query($con, $sql);
 
-                      foreach($meta AS $campanha):
+                      foreach($metas AS $meta):
                         ?>
-                        <?php if ($campanha['status'] == 1): ?>
+                        <?php if ($meta['status'] == 1): ?>
                           <tr >
-                            <td><?php echo $campanha['id']; ?></td>
-                            <td><?php echo $campanha['nome']; ?></td>
-                            <td><?php echo $campanha['descricao']; ?></td>
-                            <td><?php echo $campanha['pontos']; ?></td>
-                            <td><?php echo $campanha['objetivo']; ?></td>
-                            <?php if ($campanha['status'] == 1): ?>
-                              <td ><?php echo "Não concluida" ?></td>
+                            <td><?php echo $meta['id']; ?></td>
+                            <td><?php echo $meta['nome']; ?></td>
+                            <td><?php echo $meta['descricao']; ?></td>
+                            <td><?php echo $meta['tipo']; ?></td>
+                            <td><?php echo $meta['bonificacao']; ?></td>
+                            <td><?php echo $meta['variante_pontos']; ?></td>
+                            <td><?php echo $meta['pontos']; ?></td>
+                            <td><?php echo $meta['objetivo']; ?></td>
+                            <?php if ($meta['status'] == 1): ?>
+                              <td ><?php echo "Não concluída" ?></td>
                             <?php else: ?>
-                              <td><?php echo "Concluida" ?></td>
+                              <td><?php echo "Concluída" ?></td>
                             <?php endif; ?>
                           </tr>
                         <?php else: ?>
                           <tr class="table-success">
-                            <td><?php echo $campanha['id']; ?></td>
-                            <td><?php echo $campanha['nome']; ?></td>
-                            <td><?php echo $campanha['descricao']; ?></td>
-                            <td><?php echo $campanha['pontos']; ?></td>
-                            <td><?php echo $campanha['objetivo']; ?></td>
-                            <?php if ($campanha['status'] == 1): ?>
-                              <td ><?php echo "Não concluida" ?></td>
+                            <td><?php echo $meta['id']; ?></td>
+                            <td><?php echo $meta['nome']; ?></td>
+                            <td><?php echo $meta['descricao']; ?></td>
+                            <td><?php echo $meta['tipo']; ?></td>
+                            <td><?php echo $meta['bonificacao']; ?></td>
+                            <td><?php echo $meta['variante_pontos']; ?></td>
+                            <td><?php echo $meta['pontos']; ?></td>
+                            <td><?php echo $meta['objetivo']; ?></td>
+                            <?php if ($meta['status'] == 1): ?>
+                              <td ><?php echo "Não concluída" ?></td>
                             <?php else: ?>
                               <td><?php echo "Concluída" ?></td>
                             <?php endif; ?>
