ad9b4c5e0cf311d4aac68d7ac1af9f4d439eaeb6
Alexander Cavalheiro Becker <callfrygou@hotmail.com>
2019-06-27 15:34:39 -0300

    Creates group with 0 points

--- system/admin/teams/back_cadastro_grupo.php
+++ system/admin/teams/back_cadastro_grupo.php
@@ -8,7 +8,7 @@ $link     = "../navbar.php?folder=teams&file=tela_cadastro_grupo.php";
 
 
 $con = mysqli_connect("localhost","root","root","bd_fito");
-$formar_grupo1 = mysqli_query($con, "INSERT INTO grupos VALUES(NULL,'{$nome_grupo}','0', '{$descricao_grupo}')");
+$formar_grupo1 = mysqli_query($con, "INSERT INTO grupos VALUES(NULL,'{$nome_grupo}','0', '{$descricao_grupo}', '0')");
 echo $formar_grupo1;
 $inseriu = mysqli_affected_rows($con);
 
