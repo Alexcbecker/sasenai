9736eac7bd0a43e6f5664d3618c63a9c5302839f
Alexander Cavalheiro Becker <callfrygou@hotmail.com>
2019-06-27 12:09:05 -0300

    Selects only active users with goals for the adm register points screen

--- system/admin/tela_cadastro_pontos.php
+++ system/admin/tela_cadastro_pontos.php
@@ -40,7 +40,7 @@ $(document).ready(function(){
   include "../../database/conexao_bd.php";
   ?>
   <div class="borda" style=" margin-top:3%;">
-          <h4 style="text-align:center;">Colaboradores ativos</h4>
+          <h4 style="text-align:center;">Colaboradores com metas</h4>
           <hr>
           <table class="table table-hover overflow-y">
             <thead  class="thead-dark">
@@ -57,10 +57,31 @@ $(document).ready(function(){
             <tbody id="tabela">
               <?php
               if(isset($_POST)){
-                $sql = "SELECT * FROM colaboradores INNER JOIN metas_has_colaboradores where colaboradores.status = 0 AND colaboradores.tipo != 1";
-                $colaboradores  = mysqli_query($con, $sql);
+                // $sql = "SELECT * FROM colaboradores INNER JOIN metas_has_colaboradores where colaboradores.status = 0 AND colaboradores.tipo != 1";
+                // $colaboradores  = mysqli_query($con, $sql);
 
-                foreach($colaboradores AS $colaborador):
+                $arrayColaboradoresMetasGrupo = array();
+                $colaboradores = array();
+
+                $colaboradoresMetasGrupo = mysqli_query($con, "SELECT colaboradores.id, colaboradores.cpf, colaboradores.nome, colaboradores.tipo, colaboradores.status FROM colaboradores INNER JOIN colaboradores_has_grupos ON colaboradores_has_grupos.colaboradores_id=colaboradores.id INNER JOIN metas_has_colaboradores_has_grupos ON metas_has_colaboradores_has_grupos.colaboradores_has_grupos_id=colaboradores_has_grupos.id WHERE colaboradores.status = 0 AND colaboradores.tipo != 1 AND metas_has_colaboradores_has_grupos.status = 0");
+
+                while($linha = mysqli_fetch_array($colaboradoresMetasGrupo, MYSQLI_ASSOC)) {
+                   array_push($arrayColaboradoresMetasGrupo, $linha);
+                }
+
+                $colaboradoresMetasIndividuais = mysqli_query($con, "SELECT DISTINCT colaboradores.id, colaboradores.cpf, colaboradores.nome, colaboradores.tipo, colaboradores.status FROM colaboradores INNER JOIN metas_has_colaboradores ON metas_has_colaboradores.colaboradores_id=colaboradores.id WHERE colaboradores.status = 0 AND colaboradores.tipo != 1 AND metas_has_colaboradores.status = 0");
+                
+                while($linha2 = mysqli_fetch_array($colaboradoresMetasIndividuais, MYSQLI_ASSOC)) {
+                   array_push($colaboradores, $linha2);
+                }
+
+                for($i = 0; $i < sizeof($colaboradores); $i++){
+                  if (!in_array($colaboradores[$i], $arrayColaboradoresMetasGrupo)) { 
+                      array_push($arrayColaboradoresMetasGrupo, $colaboradores[$i]);
+                  }
+                }
+
+                foreach($arrayColaboradoresMetasGrupo AS $colaborador):
                   $idColaborador = $colaborador['id'];
               ?>
               <!-- <form action="back_tela_cadastro_pontos.php" method="POST"> -->
