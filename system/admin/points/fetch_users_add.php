1b6c60a235e5803c792a0260bf917223cec97dff
adriano <adriano.souza113@gmail.com>
2019-07-03 17:18:36 -0300

    fix points.

---
+++ system/admin/points/fetch_users_add.php
@@ -0,0 +1,31 @@
+<?php
+include '../../base_db.php';
+
+$q = <<<S
+SELECT * FROM colaboradores
+WHERE colaboradores.id NOT IN (SELECT colaboradores_id FROM metas_has_colaboradores)
+ORDER BY colaboradores.nome
+S;
+
+$r = $conn->query($q);
+
+$data = [];
+
+if ($r->num_rows > 0)
+{
+    $rowa = [];
+
+    while ($row = $r->fetch_assoc())
+    {
+        $rowa['id'] = $row['id'];
+        $rowa['name'] = $row['nome'];
+        $rowa['cpf'] = $row['cpf'];
+        $rowa['type'] = $row['tipo'] == 1 ? "Administrador" : ($row['tipo'] == 2 ? "Líder" : "Colaborador");
+
+        array_push($data, $rowa);
+    }
+}
+
+
+echo json_encode($data);
+?>
